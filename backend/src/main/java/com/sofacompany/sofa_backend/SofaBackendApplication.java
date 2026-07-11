37f5adde552d04b9c4440c85fd9e9229874ced87
Mastewal <mastewalyirdaw257@gmail.com>
2026-07-11 01:07:38 -0300

    Add Spring Boot backend scaffold

---
+++ backend/src/main/java/com/sofacompany/sofa_backend/SofaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sofacompany.sofa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sofacompany/sofa_backend/SofaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofacompany.sofa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
